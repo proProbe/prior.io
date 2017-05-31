148a88b3e5da91cd87d92058021d980f94a832f1
Huy Tran <huytr402@student.liu.se>
2017-06-01 00:10:25 +0200

    add more configurations

--- webpack.config.js
+++ webpack.config.js
@@ -15,47 +15,51 @@ if (fileSystem.existsSync(secretsPath)) {
 }
 
 var options = {
-  entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
-  },
-  output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
-  },
-  module: {
-    rules: [
-      { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/  }
-    ]
-  },
-  resolve: {
-    alias: alias,
-    extensions: [".js", ".jsx", ".css"]
-  },
-  plugins: [
-    // expose and write the allowed env vars on the compiled bundle
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
-    }),
-    new WriteFilePlugin()
-  ]
+    entry: {
+        popup: path.join(__dirname, "src", "js", "popup.js"),
+        options: path.join(__dirname, "src", "js", "options.js"),
+        background: path.join(__dirname, "src", "js", "background.js")
+    },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ },
+            { test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/ }
+        ]
+    },
+    resolve: {
+        alias: alias,
+        extensions: [".js", ".jsx", ".css"]
+    },
+    plugins: [
+        // expose and write the allowed env vars on the compiled bundle
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "popup.html"),
+            filename: "popup.html",
+            chunks: ["popup"],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "options.html"),
+            filename: "options.html",
+            chunks: ["options"],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "background.html"),
+            filename: "background.html",
+            chunks: ["background"],
+        }),
+        new WriteFilePlugin(),
+    ],
+    // When creating content_scripts, these must not be reloaded
+    chromeExtensionBoilerplate: {
+        notHotReload: [],
+    },
 };
 
 if (env.NODE_ENV === "development") {
