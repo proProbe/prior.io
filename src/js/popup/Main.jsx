2e84ff98e6f0de1680a27ae5076b11439c4d3a1b
Huy Tran <huytr402@student.liu.se>
2017-06-22 22:32:22 +0200

    fix some small formatting errors

--- src/js/popup/Main.jsx
+++ src/js/popup/Main.jsx
@@ -4,11 +4,7 @@ import UrlInput from "./UrlInput";
 import IntervalInput from "./IntervalInput";
 import Timer from "../utils/timer";
 import Socket from "../utils/socket";
-import type {
-  RequestState,
-  // Message,
-  BackgroundMessage,
-} from "../utils/types";
+import type { RequestState, BackgroundMessage } from "../utils/types";
 
 const DEFAULT_INTERVAL = 15 * Timer.minute;
 
@@ -35,7 +31,7 @@ export default class extends React.Component {
     return {
       url: "",
       interval: DEFAULT_INTERVAL,
-      requestState: {type: "None"},
+      requestState: { type: "None" },
     };
   }
 
@@ -67,12 +63,12 @@ export default class extends React.Component {
   startSearch = (): void => {
     this.socket.postMessage({
       id: "Start search",
-      data: {url: this.state.url, interval: this.state.interval},
+      data: { url: this.state.url, interval: this.state.interval },
     });
   }
 
   clearSearch = (): void => {
-    this.socket.postMessage({id: "Clear search"});
+    this.socket.postMessage({ id: "Clear search" });
   }
 
   render() {
@@ -80,7 +76,9 @@ export default class extends React.Component {
       <div>
         <UrlInput onChange={this.setUrl} url={this.state.url} />
         <IntervalInput onChange={this.setInterval} interval={this.state.interval} />
-        <button onClick={this.startSearch} disabled={this.state.requestState.type === "Start"}>Search</button>
+        <button onClick={this.startSearch} disabled={this.state.requestState.type === "Start"}>
+          Search
+        </button>
         <button onClick={this.clearSearch}>Clear</button>
       </div>
     );
